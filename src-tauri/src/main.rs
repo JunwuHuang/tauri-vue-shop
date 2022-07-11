bfa7bf1ff202b7fd6efbe5c5bb444e98f62828a6
JunwuHuang <978162622@qq.com>
2022-07-11 23:22:18 +0800

    feat: sqlite integration

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,35 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
+mod todo;
+use todo::{Todo, TodoApp};
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_todos, new_todo])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn get_todos() -> Vec<Todo> {
+    let app: TodoApp = TodoApp::new().unwrap();
+    let todos: Vec<Todo> = app.get_todos().unwrap();
+    match app.conn.close() {
+        Ok(o) => o,
+        Err(e) => panic!("{:?}", e),
+    };
+    todos
+}
+
+#[tauri::command]
+fn new_todo(todo: Todo) -> bool {
+    let app: TodoApp = TodoApp::new().unwrap();
+    let result = app.new_todo(todo);
+    match app.conn.close() {
+        Ok(o) => o,
+        Err(e) => panic!("{:?}", e),
+    };
+    result
 }
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,69 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue'
+import { invoke } from '@tauri-apps/api'
+import { nanoid } from 'nanoid/non-secure'
+
+interface Todo {
+  id: string,
+  label: string,
+  done: boolean,
+  is_delete: boolean,
+}
+
+const loading = ref(true)
+const ipt = ref('')
+let todos = ref<Todo[]>([])
+const getTodos = async () => {
+  try {
+    const list = await invoke<Todo[]>('get_todos')
+    todos.value = list
+  } catch (error) {
+    console.log(error)
+  } finally {
+    loading.value = false
+  }
+}
+onMounted(getTodos)
+const newTodo = async () => {
+  loading.value = true
+
+  try {
+    const ret = await invoke<boolean>('new_todo', {
+      todo: {
+        id: nanoid(),
+        label: ipt.value,
+        done: false,
+        is_delete: false,
+      }
+    })
+    console.log(ret)
+    if (ret) {
+      ipt.value = ''
+      getTodos()
+    }
+  } catch (error) {
+    console.log(error)
+  } finally {
+    loading.value = false
+  }
+}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <label>
+    <h2>Input Todo</h2>
+    <div>
+      <input type="text" v-model="ipt" @keyup.enter="newTodo" :readonly="loading">
+    </div>
+  </label>
+  <h2 v-if="loading">Loading...</h2>
+  <ul v-else>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.label }}
+    </li>
+  </ul>
 </template>
 
 <style>
